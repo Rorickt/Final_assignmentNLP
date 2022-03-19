fe7f85ec32ffbf88720b4d5ab7d31f75bb517c41
Rorickt <rorick.terlou@gmail.com>
2022-03-19 11:50:41 +0100

    test

---
+++ try.py
@@ -0,0 +1,25 @@
+trkdfjdkfj
+
+FileNotFoundError
+
+def test_function(foo, bar):
+    """
+
+    Args:
+        foo (_type_): _description_
+        bar (_type_): _description_
+    """
+    x = 0
+    y = 'this is a string'
+
+    for i in y:
+        print(i)
+
+    if x > 2:
+        try:
+            x+2
+        except:
+            x-2
+    
+    return y+x
+
