d7384ff2873f556cce7ce15c8044bdc81e283442
Rorickt <rorick.terlou@gmail.com>
2022-03-29 15:33:34 +0200

    final run

--- roleset.py
+++ roleset.py
@@ -6,6 +6,8 @@ def create_roleset():
     editor = Editor()
     building= ['store','house','hotel','shop', 'pharmacy', 'room', 'station',
     'airport','boat','harbor','school','cafe', 'restaurant', 'university']
+    item= ['phone','pen','knife','fork', 'spoon', 'charger', 'headphones',
+    'bottle','keys','wallet','bag','beanie', 'backpack', 'book']
     
     ###### Manner addition [variance test] ######
     roleset_template1 = '{first_name} left the {building}.'
@@ -20,13 +22,20 @@ def create_roleset():
     write_to_file('challenge_sets/roleset/roleset_leave2.csv', roleset_leave2.data, 'ARG1')
     write_to_file('challenge_sets/roleset/roleset_leave3.csv', roleset_leave2.data, 'ARG2')
 
+    ###### Manner addition [variance test] ######
+    roleset_template2 = '{first_name} left the {item} to {first_name1}.'
+    # the gold label for the challengeset is a string of all combined SR labels (stripping 'B/I-')
+    roleset_leave2 = editor.template(roleset_template2, item=item, nsamples=100, remove_duplicates=True)
+    write_to_file('challenge_sets/roleset/roleset_leave4.csv', roleset_leave2.data, 'ARG1')
+    write_to_file('challenge_sets/roleset/roleset_leave5.csv', roleset_leave2.data, 'ARG2')
+
 #### Predict sentences with AllenNLP with BiLSTM and BERT ####
 def predict_roleset():
-    files = ['roleset_leave1.csv', 'roleset_leave2.csv', 'roleset_leave3.csv']
+    files = ['roleset_leave1.csv', 'roleset_leave2.csv', 'roleset_leave3.csv', 'roleset_leave4.csv', 'roleset_leave5.csv']
     models = ['structured-prediction-srl', 'structured-prediction-srl-bert']
     for model in models:
         for file_ in files:
-            if '3' in file_:
+            if '3' in file_ or '5' in file_:
                 predictions, gold_labels, full_preds, erred_preds= predict_sents('challenge_sets/roleset/'+file_, model, 0, 5)
                 j_dumps('predictions/roleset/'+file_+'_output'+model[-4:]+'.json', full_preds)
                 j_dumps('predictions/roleset/'+file_+'_output_err'+model[-4:]+'.json', erred_preds)
